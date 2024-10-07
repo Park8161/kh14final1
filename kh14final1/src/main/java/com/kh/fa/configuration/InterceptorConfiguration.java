575af55acd5138e836aaec4b7a3024be9ee30478
Park8161 <qkraudgh55@gmail.com>
2024-10-07 12:42:58 +0900

    인터셉터 설정 변경

--- kh14final1/src/main/java/com/kh/fa/configuration/InterceptorConfiguration.java
+++ kh14final1/src/main/java/com/kh/fa/configuration/InterceptorConfiguration.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.kh.fa.interceptor.AdminInterceptor;
 import com.kh.fa.interceptor.MemberInterceptor;
-import com.kh.fa.interceptor.TestInterceptor;
 
 /*
 커스텀 설정 파일
@@ -19,11 +17,7 @@ import com.kh.fa.interceptor.TestInterceptor;
 public class InterceptorConfiguration implements WebMvcConfigurer {
 	
 	@Autowired
-	private TestInterceptor testInterceptor;
-	@Autowired
 	private MemberInterceptor memberInterceptor;
-	@Autowired
-	private AdminInterceptor adminInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -70,21 +64,6 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
 		// 다 막아놓고 일부 허용 : 화이트리스트 방식
 		// 다 허용해놓고 일부 막음 : 블랙리스트 방식
 
-		// 관리자 검사 인터셉터 설정
-		registry.addInterceptor(adminInterceptor)
-				.addPathPatterns(
-						"/admin/**",
-						"/rest/poketmon/status*", // 통게 관련 페이지
-						"/rest/book/status*", // 통게 관련 페이지
-						"/rest/emp/status*", // 통게 관련 페이지
-						"/rest/member/status*" // 통게 관련 페이지
-						)
-				.excludePathPatterns(
-						
-				);
-		
-		
-		
 		
 	}
 	
