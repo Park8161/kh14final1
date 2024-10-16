5d5b03d28f1d97bb745384ae829ed1f6b8c5fc83
Park8161 <qkraudgh55@gmail.com>
2024-10-16 16:46:40 +0900

    테이블 변경에 따른 DTO 수정

--- kh14final1/src/main/java/com/kh/fa/dto/CategoryDto.java
+++ kh14final1/src/main/java/com/kh/fa/dto/CategoryDto.java
@@ -4,7 +4,19 @@ import lombok.Data;
 
 @Data
 public class CategoryDto {
-	private int firstCategorytNo;
-	private String firstCategoryName;
+	private int categorytNo;
+	private String categoryName;
+	
+	private int categoryGroup; // 분류그룹번호
+	private Integer categoryUpper; // 상위분류, null 가능
+	private int categoryDepth; // 분류차수
+	
+	// 메소드 추가 - 대분류 확인 목적
+	public boolean isNew() { // 상위분류가 없다면 - 대분류
+		return this.categoryUpper == null;
+	}
+	public boolean isNotNew() { // 상위분류가 있다면 - 대분류 X
+		return this.categoryUpper !=null;
+	}
 	
 }
