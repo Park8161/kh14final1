572c8380f72e9173a4d97a0e5fde445144e6ac16
Park8161 <qkraudgh55@gmail.com>
2024-10-17 17:34:03 +0900

    오타 수정

--- kh14final1/src/main/java/com/kh/fa/restcontroller/ProductRestController.java
+++ kh14final1/src/main/java/com/kh/fa/restcontroller/ProductRestController.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +24,9 @@ import com.kh.fa.error.TargetNotFoundException;
 import com.kh.fa.service.AttachmentService;
 import com.kh.fa.service.TokenService;
 import com.kh.fa.vo.MemberClaimVO;
+import com.kh.fa.vo.ProductInsertRequestVO;
+import com.kh.fa.vo.ProductListRequestVO;
+import com.kh.fa.vo.ProductListResponseVO;
 
 @CrossOrigin
 @RestController
@@ -36,14 +42,12 @@ public class ProductRestController {
 	@Autowired
 	private AttachmentService attachmentService;
 	
-	
 	// 등록
-	@PostMapping("/insert")
-	public void insert(@RequestHeader("Authorization") String token, 
-					@RequestBody ProductDto productDto, 
-					@RequestPart(value = "file", required = false) List<MultipartFile> attachList) 
+	@Transactional
+	@PostMapping(value = "/insert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void insert(@RequestHeader("Authorization") String token,
+						@ModelAttribute ProductInsertRequestVO requestVO) 
 					throws IllegalStateException, IOException {
-		System.out.println("productDto : "+productDto);
 		// 토큰 변환
 		MemberClaimVO claimVO = tokenService.check(tokenService.removeBearer(token));
 		// 유효 검증
@@ -51,18 +55,35 @@ public class ProductRestController {
 		if(memberDto == null) throw new TargetNotFoundException("존재하지 않는 회원");
 		// 상품 정보 등록 - productDto에는 상품 번호와 상품등록자(판매자)가 빠져있다
 		int productNo = productDao.sequence();
+		ProductDto productDto = new ProductDto();
 		productDto.setProductNo(productNo);
 		productDto.setProductMember(claimVO.getMemberId());
+		productDto.setProductName(requestVO.getProductName());
+		productDto.setProductCategory(requestVO.getProductCategory());
+		productDto.setProductPrice(requestVO.getProductPrice());
+		productDto.setProductDetail(requestVO.getProductDetail());
+		productDto.setProductQty(requestVO.getProductQty());
 		productDao.insert(productDto);
-		
 		// 파일 등록
-		for(MultipartFile attach : attachList) {
+		for(MultipartFile attach : requestVO.getAttachList()) {
 			if(attach.isEmpty()) continue; // 파일 없으면 스킵
 			
 			int attachmentNo = attachmentService.save(attach);
 			productDao.connect(productNo, attachmentNo);
-		}
+		}		
+	}
+	
+	// 목록 조회
+	@PostMapping("/list")
+	public ProductListResponseVO list(@RequestBody ProductListRequestVO requestVO){
 		
+		int count = productDao.countWithPaging(requestVO);
+		boolean last = requestVO.getEndRow() == null || count <= requestVO.getEndRow();
+		ProductListResponseVO responseVO = new ProductListResponseVO();
+		responseVO.setProductList(productDao.selectListByPaging(requestVO));
+		responseVO.setCount(count);
+		responseVO.setLast(last);
+		return responseVO;
 	}
 	
 	
