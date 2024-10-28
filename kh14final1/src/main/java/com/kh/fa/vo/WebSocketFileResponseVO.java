e4af53c4ae51b6bd2dea34d58f76a8e07425f0a3
ChaeSuJin <univer7519@naver.com>
2024-10-28 18:27:46 +0900

    채팅방 파일 업로드 vo 추가

---
+++ kh14final1/src/main/java/com/kh/fa/vo/WebSocketFileResponseVO.java
@@ -0,0 +1,17 @@
+package com.kh.fa.vo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class WebSocketFileResponseVO {
+	private final String type = "file";
+	private String senderMemberId; // 발신자
+	private String senderMemberLevel; // 발신자의 등급
+	private LocalDateTime time; // 발신 시각
+	private byte[] file; // 보낸 파일 데이터
+}
